806ee3bd7ba22854d7355a8dd855a1551f4dff97
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2024-04-11 09:27:03 +0200

    creación estructura

--- runApp.mjs
+++ runApp.mjs
@@ -1,10 +1,25 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser'; 
+import path from 'path';
+import { fileURLToPath } from 'url'; 
+import dotenv from 'dotenv';
+
+dotenv.config();
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename);
 
 const app = express();
+
+
+app.use(express.static(path.join(__dirname, '/browser')));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
-app.listen(3000, () => {
-    console.log(`Example app listening at http://localhost:3000`);
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`);
 });
