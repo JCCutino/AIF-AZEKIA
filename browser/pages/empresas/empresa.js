20356b72d336280d8b1ca22170f9b0c38abafcce
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2024-04-11 12:28:58 +0200

    Funcion para elimar empresa

--- browser/pages/empresas/empresa.js
+++ browser/pages/empresas/empresa.js
@@ -1,6 +1,6 @@
-async function anadirEmpresa(empresa) {
+async function agregarEmpresa(empresa) {
     try {
-        const response = await fetch('/anadirEmpresa', {
+        const response = await fetch('/agregarEmpresa', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -39,27 +39,44 @@ async function obtenerEmpresasAPI(pagina = 1, resultadosTotales = 10) {
     }
 }
 
-const empresa = {
-    empresaCod: 'E004',
-    CIF: '54635264R',
-    razonSocial: 'AZEKIA',
-    direccion: 'Avenida SEVILLA 123',
-    CP: '41720',
-    municipio: 'Los palacios'
-};
+async function eliminarEmpresa(empresaCod) {
+    try {
+        const response = await fetch('/eliminarEmpresa', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({empresaCod})
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log('Empresa eliminada correctamente:', data);
+        } else {
+            console.error('Error al eliminar empresa:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error al eliminar empresa:', error.message);
+    }
+}
+
+
 
 async function main() {
     try {
         const empresa = {
-            empresaCod: 'E004',
-            CIF: '54635264R',
-            razonSocial: 'AZEKIA',
-            direccion: 'Avenida SEVILLA 123',
+            empresaCod: 'E005',
+            CIF: '64672383T',
+            razonSocial: 'Rusvel',
+            direccion: 'Avenida SEVILLA 456',
             CP: '41720',
             municipio: 'Los palacios'
         };
+        const empresaCod = "E003";
 
-        await anadirEmpresa(empresa);
+
+       // await anadirEmpresa(empresa);
+       await obtenerEmpresasAPI();
+        await eliminarEmpresa(empresaCod);
         await obtenerEmpresasAPI();
     } catch (error) {
         console.error('Error en la ejecución principal:', error);
