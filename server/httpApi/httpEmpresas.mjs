a031cac8d08e7496c1ad5652917788b0e6dbe50b
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2024-04-16 09:43:33 +0200

    Comprobamos los tipos de empresa para ver que en este caso el usuario nos ha pasado un string

--- server/httpApi/httpEmpresas.mjs
+++ server/httpApi/httpEmpresas.mjs
@@ -32,34 +32,40 @@ class HttpEmpresas {
         // res.send(200, { err: false, empresa: { datos de la empresa guardada } });
         // res.send(200, { err: true, errmsg: 'Ya existe una empresa con ese CIF' });
         // res.send(500) (<>200) 
-            // AUTORIZAR
-            // ¿el usuario se había autenticado?
-            // ¿el usuario tiene pemiso para llamar a esta api?
-            // ¿el usuario tiene permiso para llamar a esta api con estos datos (para un ámbito)?
-            // SANITIZAR:
-            // body trae un OBJETO llamado "empresa"
-            // a partir de aquí podría hacerlo una función "validate" de la lib
-            // empresaCod es una string de máximo 20 caracteres
-            // empresaCif es una string de máximo 20 caracteres (y es un CIF válido)
-            // etc. etc..
-            // empresa no trae ningún atributo que no esté previsto
-            // si todo ok...
-            try {
-                const empresa = req.body.empresa;
-            
-                if (Object.prototype.toString.call(empresa) === '[object Object]') {
+        // AUTORIZAR
+        // ¿el usuario se había autenticado?
+        // ¿el usuario tiene pemiso para llamar a esta api?
+        // ¿el usuario tiene permiso para llamar a esta api con estos datos (para un ámbito)?
+        // SANITIZAR:
+        // body trae un OBJETO llamado "empresa"
+        // a partir de aquí podría hacerlo una función "validate" de la lib
+        // empresaCod es una string de máximo 20 caracteres
+        // empresaCif es una string de máximo 20 caracteres (y es un CIF válido)
+        // etc. etc..
+        // empresa no trae ningún atributo que no esté previsto
+        // si todo ok...
+        try {
+            const empresa = req.body.empresa;
 
-                    const atributosRequeridos = ['empresaCod', 'CIF', 'razonSocial', 'direccion', 'CP', 'municipio'];
-                    const atributosEmpresa = Object.keys(empresa);
-            
-                    const atributosFaltantes = atributosRequeridos.filter(key => !atributosEmpresa.includes(key));
-                    const atributosExtra = atributosEmpresa.filter(key => !atributosRequeridos.includes(key));
-            
-                    if (atributosFaltantes.length === 0 && atributosExtra.length === 0) {
-                        // Todas las claves requeridas están presentes y no hay claves adicionales
-            
+            if (Object.prototype.toString.call(empresa) === '[object Object]') {
+
+                const atributosRequeridos = ['empresaCod', 'CIF', 'razonSocial', 'direccion', 'CP', 'municipio'];
+                const atributosEmpresa = Object.keys(empresa);
+
+                const atributosFaltantes = atributosRequeridos.filter(key => !atributosEmpresa.includes(key));
+                const atributosExtra = atributosEmpresa.filter(key => !atributosRequeridos.includes(key));
+
+                if (atributosFaltantes.length === 0 && atributosExtra.length === 0) {
+                    // Todas las claves requeridas están presentes y no hay claves adicionales
+
+                    const atributosInvalidos = Object.keys(empresa).filter(key => typeof empresa[key] !== 'string');
+
+
+                    if (atributosInvalidos.length === 0) {
+                        // Todos los valores son del tipo de datos adecuado
+
                         const empresaValida = await libEmpresas.verificarEmpresa(empresa);
-            
+
                         if (!empresaValida) {
                             res.status(200).send({ err: true, errmsg: 'La empresa no es válida' });
                         } else {
@@ -67,21 +73,28 @@ class HttpEmpresas {
                             res.status(200).send({ err: false, empresa: resultado });
                         }
                     } else {
+                        // Si algunos valores no son del tipo de datos adecuado, enviar un mensaje de error
                         res.status(200).send({
                             err: true,
-                            errmsg: `El objeto de empresa debe contener exactamente las siguientes claves: ${atributosRequeridos.join(', ')}.`
+                            errmsg: `Los siguientes atributos de empresa deben ser del tipo varchar: ${invalidValues.join(', ')}.`
                         });
                     }
                 } else {
-                    // Si req.body.empresa no es un objeto válido, enviar un mensaje de error
-                    res.status(400).send({ err: true, errmsg: 'El cuerpo de la solicitud no contiene un objeto de empresa válido' });
+                    res.status(200).send({
+                        err: true,
+                        errmsg: `El objeto de empresa debe contener exactamente las siguientes claves: ${atributosRequeridos.join(', ')}.`
+                    });
                 }
-            } catch (err) {
-                console.error('Error al agregar empresa:', err);
-                res.status(500).send({ err: true, errmsg: 'Error interno del servidor' });
+            } else {
+                // Si req.body.empresa no es un objeto válido, enviar un mensaje de error
+                res.status(200).send({ err: true, errmsg: 'El cuerpo de la solicitud no contiene un objeto de empresa válido' });
             }
-            
+        } catch (err) {
+            console.error('Error al agregar empresa:', err);
+            res.status(500).send({ err: true, errmsg: 'Error interno del servidor' });
         }
+
+    }
 
     async postActualizarEmpresa(req, res) {
         try {
