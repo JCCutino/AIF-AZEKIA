1b8ff5001542f459154a8f4c8df6b506995fe9e5
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2024-04-16 08:59:10 +0200

    Comprobamosi el body manda un objeto y comprobamos  los atributos/claves que tiene el objeto

--- server/httpApi/httpEmpresas.mjs
+++ server/httpApi/httpEmpresas.mjs
@@ -46,18 +46,41 @@ class HttpEmpresas {
             // si todo ok...
             try {
                 const empresa = req.body.empresa;
-                const empresaValida = await libEmpresas.verificarEmpresa(empresa);
-        
-                if (!empresaValida) {
-                    res.status(200).send({ err: true, errmsg: 'La empresa no es válida' });
+            
+                if (Object.prototype.toString.call(empresa) === '[object Object]') {
+
+                    const atributosRequeridos = ['empresaCod', 'CIF', 'razonSocial', 'direccion', 'CP', 'municipio'];
+                    const atributosEmpresa = Object.keys(empresa);
+            
+                    const atributosFaltantes = atributosRequeridos.filter(key => !atributosEmpresa.includes(key));
+                    const atributosExtra = atributosEmpresa.filter(key => !atributosRequeridos.includes(key));
+            
+                    if (atributosFaltantes.length === 0 && atributosExtra.length === 0) {
+                        // Todas las claves requeridas están presentes y no hay claves adicionales
+            
+                        const empresaValida = await libEmpresas.verificarEmpresa(empresa);
+            
+                        if (!empresaValida) {
+                            res.status(200).send({ err: true, errmsg: 'La empresa no es válida' });
+                        } else {
+                            const resultado = await libEmpresas.agregarEmpresa(empresa);
+                            res.status(200).send({ err: false, empresa: resultado });
+                        }
+                    } else {
+                        res.status(200).send({
+                            err: true,
+                            errmsg: `El objeto de empresa debe contener exactamente las siguientes claves: ${atributosRequeridos.join(', ')}.`
+                        });
+                    }
                 } else {
-                    const resultado = await libEmpresas.agregarEmpresa(empresa);
-                    res.status(200).send({ err: false, empresa: resultado });
+                    // Si req.body.empresa no es un objeto válido, enviar un mensaje de error
+                    res.status(400).send({ err: true, errmsg: 'El cuerpo de la solicitud no contiene un objeto de empresa válido' });
                 }
             } catch (err) {
                 console.error('Error al agregar empresa:', err);
                 res.status(500).send({ err: true, errmsg: 'Error interno del servidor' });
             }
+            
         }
 
     async postActualizarEmpresa(req, res) {
