b2b7a6a9dcf1e7b04084b41c13aca9aca317e04d
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2024-05-07 08:54:28 +0200

    Creación de usuario y login

--- server/appLib/dbConexion.mjs
+++ server/appLib/dbConexion.mjs
@@ -9,9 +9,9 @@ class DbConexion {
 
     async conectarDB() {
         const sqlConfig = {
-            user: process.env.DB_NAME,
+            user: process.env.DB_USER,
             password: process.env.DB_PWD,
-            database: process.env.DB_DATABASE,
+            database: process.env.DB_NAME,
             server: process.env.DB_SERVER,
             pool: {
                 max: 10,
